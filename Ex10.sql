2cfe3b38a9277f89145888e828acc8042246a6a7
Litterial <yungice12@gmail.com>
2019-04-30 15:06:17 -0500

    added comment on Ex10

--- Ex10.sql
+++ Ex10.sql
@@ -2,6 +2,10 @@
 --
 -- 10. Show the average each person pays for rent each year.
 
+
+--This is commented out so I don't run this accidently. It turns the rent from monthly to yearly--
+--update address set rent=(rent*12)--
+
 select name,avg(rent) as Average_cost_of_yearly_rent from people
 inner join address
 on address.id=people.id
