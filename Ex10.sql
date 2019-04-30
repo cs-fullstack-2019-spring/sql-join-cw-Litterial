34cd3d7b23f4c576ef72747e4b06f7e6fda733e9
Litterial <yungice12@gmail.com>
2019-04-30 15:02:47 -0500

    I forgot to return the average. Opps

--- Ex10.sql
+++ Ex10.sql
@@ -2,7 +2,8 @@
 --
 -- 10. Show the average each person pays for rent each year.
 
-select name,address,rent from people
+select name,avg(rent) as Average_cost_of_yearly_rent from people
 inner join address
 on address.id=people.id
+group by name
 order by name asc;
